d01fba869f1929fee5ccb8c021d36fbabcff0c46
seungyeon.bae <bsyeon015@gmail.com>
2022-07-25 16:10:03 +0900

    :truck: 컴포넌트 파일 이동

---
+++ src/components/Card.js
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "styled-components";
+
+const Item = styled.div`
+    width: 33.33333333%;
+    height: 450px;
+    margin-bottom: 100px;
+    padding: 0 10px;
+    border-radius: 5px;
+`;
+
+const ThumbnailWrapper = styled.div`
+    width: 100%;
+    height: 350px;
+    margin-bottom: 10px;
+    border-radius: 5px;
+    overflow: hidden;
+`;
+
+const Thumbnail = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+    &:hover {
+        transform: scale(1.03);
+        cursor: pointer;
+    }
+`;
+
+const ContentWrapper = styled.article`
+    display: flex;
+    flex-flow: column;
+    flex-wrap: wrap;
+    text-align: center;
+`;
+
+const Title = styled.h1`
+    margin-bottom: 3px;
+    font-size: 20px;
+`
+
+const Price = styled.strong`
+    margin-bottom: 15px;
+    font-size: 17px;
+`
+
+const Description = styled.p`
+    font-size: 17px;
+`
+
+function Card(props) {
+	return (
+        <Item>
+            <ThumbnailWrapper>
+                <Thumbnail i={props.i} src={process.env.PUBLIC_URL + `/static/img/product/${props.i + 1}.jpg`} alt={`${props.product.title}`}></Thumbnail>
+            </ThumbnailWrapper>
+            <ContentWrapper>
+                <Title>{props.product.title}</Title>
+                <Price>{props.product.price}원</Price>
+                <Description>{props.product.content}</Description>
+            </ContentWrapper>
+        </Item>
+	);
+}
+
+export default Card;
---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import { useNavigate } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+
+const HeaderContent = styled.div`
+    display: flex;
+    align-items: flex-end;
+    width: 100%;
+    padding: 30px 0 30px 50px;
+    border-bottom: 1px solid #CCC;
+    background-color: #FFF;
+`
+
+const Logo = styled.h1`
+    margin-right: 50px;
+    font-size: 40px;
+`
+
+const MenuList = styled.ul`
+    display: flex;
+    padding-bottom: 5px;
+    font-size: 23px;
+`;
+
+const Menu = styled.li`
+    margin-right: 40px;
+    cursor: pointer;
+`;
+
+function Header() {
+    const navigate = useNavigate();
+
+    return (
+        <header>
+            <HeaderContent>
+                <Logo>ohsul</Logo>
+
+                <nav>
+                    <MenuList>
+                        <Menu onClick={() => { navigate("/") }}>
+                            메인
+                        </Menu>
+                        <Menu onClick={() => { navigate("/detail") }}>
+                            상세보기
+                        </Menu>
+                    </MenuList>
+                </nav>
+            </HeaderContent>
+        </header>
+    );
+}
+
+export default Header;
