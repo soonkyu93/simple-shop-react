23d805cf1c36b85a4c22f21dacf4072f52e14d82
seungyeon.bae <bsyeon015@gmail.com>
2022-07-26 23:02:48 +0900

    :lipstick: 버튼 스타일 추가

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    display: inline-block;
+    padding: 5px 10px;
+    color: #fff;
+    font-size: 20px;
+    border: none;
+    border-radius: 5px;
+    background-color: #000;
+    cursor: pointer;
+`;
+
+export default Button;
